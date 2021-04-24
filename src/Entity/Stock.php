7b64a46d82b462e25ed1153628c7479a14cef9de
Adarsh Manickam <adarsh.apple@icloud.com>
2021-04-24 11:10:28 +0530

    Added Stock entity

---
+++ src/Entity/Stock.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\StockRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StockRepository::class)
+ */
+class Stock
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private string $sku;
+
+    /**
+     * @ORM\Column(type="string", length=5)
+     */
+    private string $branch;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private float $stock;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getSku(): string
+    {
+        return $this->sku;
+    }
+
+    public function setSku(string $sku): self
+    {
+        $this->sku = $sku;
+
+        return $this;
+    }
+
+    public function getBranch(): string
+    {
+        return $this->branch;
+    }
+
+    public function setBranch(string $branch): self
+    {
+        $this->branch = $branch;
+
+        return $this;
+    }
+
+    public function getStock(): float
+    {
+        return $this->stock;
+    }
+
+    public function setStock(float $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+}
